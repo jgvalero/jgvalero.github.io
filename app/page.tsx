02c858ad56eab6c9bc4063e1820d7a2016f6233c
Jose Valero <jvalero000@gmail.com>
2023-11-30 20:09:15 -0700

    Add hero component and update favicon

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,25 @@
+import Image from "next/image";
+
 export default function Home() {
-  return <p>Welcome to my website!</p>;
+  return (
+    <main>
+      <Hero />
+    </main>
+  );
+}
+
+function Hero() {
+  return (
+    <section className="hero">
+      <div className="image-container">
+        <Image src="/me.png" alt="Me" width={500} height={500} />
+      </div>
+
+      <span className="hero-description">
+        Hey, I&apos;m Jose! I am a software engineer proficient in Python, Java,
+        and C/C++. I am currently learning web development! My favorite hobbies
+        are listening to music, building computers, and working on projects!
+      </span>
+    </section>
+  );
 }
